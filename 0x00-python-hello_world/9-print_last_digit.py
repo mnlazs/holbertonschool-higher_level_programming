086885583c442eea089cc4cda70d13c9cad10979
manuel <mnl.azs@gmail.com>
2023-01-10 22:57:23 +0000

    lol

---
+++ 0x00-python-hello_world/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyhton3
+def print_last_digit(numer):
+    if number < 0:
+        print("{}".format(-number % 10), end='')
+        return -number % 10
+    else:
+        print("{}".format(number % 10), end='')
+        return number % 10r/bin
