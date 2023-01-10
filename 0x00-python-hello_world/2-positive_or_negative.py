09ecf6174708e826a925790ca041c5a7d73837ae
manuel <mnl.azs@gmail.com>
2023-01-10 17:05:02 +0000

    loaded

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -2,9 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("{:d} The number", number, "is positive.")
+    print("{:d} is positive".format(number))
 elif number == 0:
-    print("{:d} The number", number, "is zero.")
-else numer:
-    print("{:d} The number", number, "is negative.")
-
+    print("{:d} is zero".format(number))
+else:
+    print("{:d} is negative".format(number))
