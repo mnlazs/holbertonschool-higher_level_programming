e72701a01e1506b26d21004b764c5c499f09537b
manuel <mnl.azs@gmail.com>
2023-01-11 21:36:50 +0000

    lol

--- 0x01-python-import_modules/5-raise_exception_msg.py
+++ 0x01-python-import_modules/5-raise_exception_msg.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def raise_exception_msg(message="Vuelve a casa"):
+def raise_exception_msg(message="C is fun"):
     raise TypeError("This function raises a TypeError exception.")
 
