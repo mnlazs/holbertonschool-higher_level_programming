4eb97653ffab9894f6c799dc1bc4c2c78975e140
manuel <mnl.azs@gmail.com>
2023-01-13 21:33:35 +0000

    lol

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b()):
-first_a = tuple_a[0] if len(tuple_a) > 0 else 0
-    first_b = tuple_b[0] if len(tuple_b) > 0 else 0
-    second_a = tuple_a[1] if len(tuple_a) > 1 else 0
-    second_b = tuple_b[1] if len(tuple_b) > 1 else 0
-    return (first_a + first_b, second_a + second_b)
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = validate_tuple(tuple_a)
+    tuple_b = validate_tuple(tuple_b)
+
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+
+
+def validate_tuple(_tuple=()):
+    if len(_tuple) < 2:
+        if len(_tuple) == 1:
+            _tuple = (_tuple[0], 0)
+        elif len(_tuple) == 0:
+            _tuple = (0, 0)
+    elif len(_tuple) > 2:
+        _tuple = (_tuple[0], _tuple[1])
+
+    return _tuple
