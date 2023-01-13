90e8aed77b153c88a7eafbaeba355bfc040d573e
manuel <mnl.azs@gmail.com>
2023-01-13 21:14:50 +0000

    update

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    for i in range(len(my_string) - 2):
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            my_string = my_string[:i] + "" + my_string[i+1:]
-    return (my_string)
+	new_str = ""
+	for i in my_string:
+		if i != "C" and i != "c"
+		new_str += i
+	return new_str
