ede6dc2c66a21dedb5bdceeaf639abd3e37ce0f2
manuel <mnl.azs@gmail.com>
2023-01-16 17:14:14 +0000

    lol

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+"""Square Class
+This class don't do anything for the moment
+"""
+
+
 class Square:
-"""
-A class that define a square by:
--private instance attribute: suze
--Instantiation with size (no type/value verification)
-"""
-def __init__(self, size):
-"""
-Initialize the square with a given size
-:param size: The size of the square (int or float)
-"""
-self.size = size
+    pass
