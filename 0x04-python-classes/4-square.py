a0f933d5060292c4846a4646aa1ad88ca6e867f7
manuel <mnl.azs@gmail.com>
2023-01-17 22:54:53 +0000

    load

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -1,29 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/pyhton3
+"""
+Square Class: defines a square by : (based on 2-square.py)
+"""
+
+
 class Square:
+    """ class Square that defines a square """
     def __init__(self, size=0):
+        """ Initialize attributes"""
         self.size = size
 
     @property
     def size(self):
-        return self._size
+        """ gets the size"""
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self._size = value
+        """ sets the size with safe Assignment"""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if (value < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
-        return self._size ** 2
-
-    def my_print(self):
-        if self._size == 0:
-            print()
-        else:
-            for i in range(self._size):
-                for j in range(self._size):
-                    print("#", end="")
-                print()
-
+        """ Return the area of the square"""
+        return (self.__size * self.__size)
