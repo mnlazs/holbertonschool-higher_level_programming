01f2243aa05bbf08b4d0a947671f7c8ec84aed56
manuel <mnl.azs@gmail.com>
2023-01-17 22:47:58 +0000

    load

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -11,7 +11,7 @@ Class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self._size = value
 
