ea4bec70eb4719955fecd7a70067426d62f012c8
manuel <mnl.azs@gmail.com>
2023-01-17 22:59:16 +0000

    load

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square:
     @size.setter
     def size(self, value):
         """ sets the size with safe Assignment"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if (value < 0):
             raise ValueError('size must be >= 0')
