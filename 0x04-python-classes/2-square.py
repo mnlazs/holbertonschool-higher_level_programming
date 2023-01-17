080a599cd28e9793a3f87dffab5c7c17de346ca0
manuel <mnl.azs@gmail.com>
2023-01-17 19:32:12 +0000

    load

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""creates class Square with private instance attribute size"""
+"""Square Class
+A Square Class
+"""
 
 
 class Square:
-    """defines class and
-instantiates private instance attribute size with validation."""
 
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
+        """__init__
+        The __init__ method initializes the size value of the square.
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+        Raises:
+            TypeError: If `size` type is not `int`.
+            ValueError: If `size` is less than `0`.
+        """
 
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
