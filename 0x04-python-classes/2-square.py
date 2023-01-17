c1c98953b5c6643b92171cd51acd3b615cca6cb6
manuel <mnl.azs@gmail.com>
2023-01-17 18:19:25 +0000

    load

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square
+"""Size Class"""
+    def __inti__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must b an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
