8bf44cb49fd909a397a954d50f0204bcd333be6f
manuel <mnl.azs@gmail.com>
2023-01-17 22:34:18 +0000

    ol

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Square Class:defines a square by: (based on 1-square.py)
-"""
+def area(self):
+    return self.__size ** 2
 
+def getSize(self):
+    return self.__size
 
-class Square:
-"""defines class and instantiates private instance attribute size with validation.
-"""
+def setSize(self, size):
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
 
-    def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
