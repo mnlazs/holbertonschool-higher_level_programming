665586d03a6652c48c6b140a62a860c1792f86b6
manuel <mnl.azs@gmail.com>
2023-01-18 18:12:47 +0000

    lol

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -7,15 +7,12 @@ class Square:
 
     def __init__(self, size=0):
         """ initialize variables """
-
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if (size < 0):
+        if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
-        """Funtion definition
-        """
-
-    return self.__size ** 2
+        """Funtion definition"""
+        return self.__size ** 2
