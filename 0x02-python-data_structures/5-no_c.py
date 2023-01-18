a159cbc47b4c4a4768b5635eee265e5814d4175d
manuel <mnl.azs@gmail.com>
2023-01-18 00:51:23 +0000

    load

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-#def no_c(my_string):
-#"""Function that removes all c and C characters from a string"""   
-#    new_string = ""
-#    for i in range(len(my_string)):
-#        if my_string[i] != 'c' and my_string[i] != 'C':
-#            new_string += my_string[i]
-#    return new_string
-#!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
     for c in my_string:
