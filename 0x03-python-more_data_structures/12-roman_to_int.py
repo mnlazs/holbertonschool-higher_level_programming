ae9daebee5be8d7c71d686c9f402e155bc7bfbc7
manuel <mnl.azs@gmail.com>
2023-01-18 00:34:42 +0000

    load

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,45 @@
-#!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+#!/usr/bin/pyhton3
+def roma_to_int(roman_string):
+    # Fail checks, none, not a string
+    if not roman_string:
         return 0
-    roman = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
+    if not isinstance(roman_string, str):
+        return 0
+    if not roman_string.isupper():
+        return 0
+    # Dictionary for roman numerals
+    r_dict = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
     result = 0
-    i = 0
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman:
-            result += roman[roman_string[i:i+2]]
-            i += 2
-        else:
-            result += roman[roman_string[i]]
-            i += 1
+    temp = list(roman_string)
+    # Concat 4 and 9s
+    if len(temp) > 1:
+        idx = 0
+        for i in temp:
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'V':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'X':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            idx += 1
+    # Search in dict for correct numbers and add
+    for k, v in r_dict.items():
+        for index in temp:
+            if index == k:
+                result += v
     return result
