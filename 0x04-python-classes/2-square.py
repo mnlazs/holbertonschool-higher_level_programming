04f338804f55af5b83bc9141a69a2b588bb751a2
manuel <mnl.azs@gmail.com>
2023-01-18 16:03:44 +0000

    lol

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -13,3 +13,7 @@ class Square:
         if (size < 0):
             raise ValueError('size must be >= 0')
         self.__size = size
+  def area(self):
+    """Funtion definition
+    """
+        return (self.__size **2)
