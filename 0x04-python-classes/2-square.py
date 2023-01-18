fac7adea7f69a33b173695c410591b9b1ddeaeda
manuel <mnl.azs@gmail.com>
2023-01-18 16:16:33 +0000

    lol

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -3,7 +3,6 @@
 Square Class: Define a Square with private instance
 """
 
-
 class Square:
     """ class Square that defines a square
     """
@@ -17,8 +16,8 @@ class Square:
         if (size < 0):
             raise ValueError('size must be >= 0')
         self.__size = size
-  
-    def area(self):
+
+def area(self):
         """Funtion definition
         """
 
