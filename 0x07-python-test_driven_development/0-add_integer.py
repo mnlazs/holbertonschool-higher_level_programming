3777e6686819f68186972fbe0f19e46f8df44cc4
manuel <mnl.azs@gmail.com>
2023-01-19 20:41:59 +0000

    load

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""This is the module comment
+"""
+
+
 def add_integer(a, b=98):
+    """"Adding two intergert or maybe flots"""
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
