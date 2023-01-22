66a026ffe96cfcd155ea27185a8934385c19cb80
manuel <mnl.azs@gmail.com>
2023-01-22 21:25:49 +0000

    load

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """
-    Modulo Comments
+Function to print a phrase
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """ print my first and last name """
-    str_error = "first_name must be a string or last_name must be a string"
-    if type(first_name) is not str or type(last_name) is not str:
-        raise TypeError(str_error)
-    print("My name is", first_name, last_name)
+    """
+    Prints a phrase with:
+    first_name: string to be printed
+    last_name: second string to be printed
+    Return: A formatted output with the strings.
+    """
+    if type(first_name) not in [str]:
+        raise TypeError("first_name must be a string")
+    if type(last_name) not in [str]:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
