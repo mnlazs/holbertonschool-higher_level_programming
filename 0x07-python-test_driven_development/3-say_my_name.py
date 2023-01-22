aa0a08719bd4df53bfb79925dae2429140cddf81
manuel <mnl.azs@gmail.com>
2023-01-22 21:17:19 +0000

    loa

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 """
-    Insert here module comment
-    Write a function that prints My name is <first name> <last name>
-    Prototype: def say_my_name(first_name, last_name=""):
-    first_name and last_name must be strings otherwise,
-    raise a TypeError exception with the message
-    first_name must be a string or last_name must be a string
-    You are not allowed to import any module
+    Modulo Comments
 """
 
 
