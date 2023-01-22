8f266726ae8ade6830c63d599f246d3b82a49599
manuel <mnl.azs@gmail.com>
2023-01-22 22:33:51 +0000

    lo

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -16,5 +16,3 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) not in [str]:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
-
-
