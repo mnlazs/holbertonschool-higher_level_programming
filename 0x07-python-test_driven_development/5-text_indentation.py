c78fddeae9cc95b6edb4a91e81c342d3b17e41d9
manuel <mnl.azs@gmail.com>
2023-01-23 06:56:28 +0000

    load

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,21 +6,21 @@ Function to replace some characters with '\n\n'
 
 def text_indentation(text):
     """
-    Prints a text with 2 new lines after some characters.
+    Prints a text with 2 new lines after each of these characters: ., ? and :
     """
 
+    # check if input is a string
     if type(text) is not str:
-        raise TypeError("text must be a string")    
-    tmp = text.replace(".", ".\n\n")
-    tmp = tmp.replace(":", ":\n\n")
-    tmp = tmp.replace("?", "?\n\n")
-    tmp = tmp.replace(";", ";\n\n")
-    tmp = tmp.replace("!", "!\n\n")
-    p = tmp.splitlines(True)
-    ls_strip = []
-    for l in p:
-        if l == "\n":
-            ls_strip.append("\n")
+        raise TypeError("text must be a string")
+    # check if input is not empty
+    if text is None or text.strip() == "":
+        raise ValueError("text can not be None or empty")
+    new_text = ""
+    for i, c in enumerate(text):
+        if c in ".?:":
+            new_text += c + "\n\n"
+            if i+1 < len(text) and text[i+1] == " ":
+                continue
         else:
-            ls_strip.append(l.lstrip())
-    return("".join(ls_strip))
+            new_text += c
+    print(new_text)
