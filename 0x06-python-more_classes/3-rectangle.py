c27def16f52b5944d324a78c489f8f91b23efaa0
manuel <mnl.azs@gmail.com>
2023-01-24 18:40:38 +0000

    load

---
+++ 0x06-python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Program that defines a Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Defines methods to create attributes.
+    """
+
+    def __init__(self, width=0, height=0):
+        """Init the object's atribute"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width and raise"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height and raise"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Prints the rectangle with str magic method"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height - 1):
+            print('#' * self.__width)
+        print('#' * self.__width, end='')
+        return ""
+    
