77796c3f113f94cea52a137ce3c0f59f6f10ed87
manuel <mnl.azs@gmail.com>
2023-01-25 16:19:23 +0000

    lolg

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value >= 0:
             raise ValueError("width must be >= 0")
         self._width = value
 
@@ -45,6 +45,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value >= 0:
             raise ValueError("height must be >= 0")
         self._height = value
