b26462545af03e69e81d0c8b0482d2695d27af0e
manuel <mnl.azs@gmail.com>
2023-01-26 18:55:59 +0000

    lol

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,9 +4,9 @@ Module with the method Buscar
 """
 
 
-def buscar(objeto):
+def lookup(obj:
     """
     Funcion que retorna el directorio de un objeto
     """
-    atributos = dir(objeto)
+    atributos = dir(obj)
     return atributos
