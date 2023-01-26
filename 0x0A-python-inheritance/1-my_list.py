de786e8f166f4f0bf8114e68b05a3e4214d45c9e
manuel <mnl.azs@gmail.com>
2023-01-26 19:17:52 +0000

    lol

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Clase que imprime una lista
+"""
+
+
+class MyList(list):
+    """modulo que imprime una lista heredada en orden
+"""
+
+
+def print_sorted(self):
+        print(sorted(self))
