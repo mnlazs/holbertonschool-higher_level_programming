462d0f191d16f68fb0b1f6f307113d6098c6cc94
manuel <mnl.azs@gmail.com>
2023-01-26 21:11:47 +0000

    lol

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Get if a object is exactly an instance of the \
+    specified class ; otherwise False
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function that returns True if the object if an instance of\
+       exactly an instance of the specified class \
+       ; otherwise False"""
+    return isinstance(obj, a_class) or (hasattr(obj, '__class__') and issubclass(obj.__class__, a_class))
