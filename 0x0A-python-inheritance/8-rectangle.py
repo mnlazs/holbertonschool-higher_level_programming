548a5a539aadecc9d622b3d0cc131dc7e3701bc4
manuel <mnl.azs@gmail.com>
2023-01-27 16:58:52 +0000

    lol

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+   6-base_geometry.py module
+   Class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        """Public instance method: def area(self)"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method:\
+           def integer_validator(self, name, value):\
+           that validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseException):
+    """Class Rentacle that inherits from BaseException"""
+    def __init__(self, width, height):
+        self.integer_validator("widht", width)
+        self.__width = width
+        self.__integer_validator("height", height)
+        self.__height = height
