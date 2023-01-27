6ead772dc2b041b8b8fc357a6d4c0c87ffaddff2
manuel <mnl.azs@gmail.com>
2023-01-27 17:01:14 +0000

    lol

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,6 +2,12 @@
 """
    6-base_geometry.py module
    Class BaseGeometry.
+   Public instance method: \
+   def area(self): that raises an Exception with\
+   the message area() is not implemented.
+   Public instance method:\
+   def integer_validator(self, name, value):\
+   that validates value
 """
 
 
@@ -9,8 +15,9 @@ class BaseGeometry:
     """Class BaseGeometry"""
 
     def area(self):
-        """Public instance method: def area(self)"""
-
+        """Public instance method: def area(self): \
+           that raises an Exception with\
+           the message area() is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -18,20 +25,20 @@ class BaseGeometry:
            def integer_validator(self, name, value):\
            that validates value"""
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
-class Rectangle(BaseException):
-    """Class Rentacle that inherits from BaseException"""
+class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
         """Instantiation with width and height: \
         def __init__(self, width, height)
         width and height must be positive integers,\
         validated by integer_validator"""
 
-        self.integer_validator("widht", width)
+        self.integer_validator("width", width)
         self.__width = width
-        self.__integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
