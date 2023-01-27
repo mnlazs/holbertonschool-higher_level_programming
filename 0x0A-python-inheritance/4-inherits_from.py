6867002c20716c3bb39da4399be04078a33126be
manuel <mnl.azs@gmail.com>
2023-01-27 15:58:43 +0000

    lol

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-    This funtion get the object is an instance 
+    This funtion get the object is an instance
     of a class that inherited (directly or indirectly) from the specified class
 """
 
 
 def inherits_from(obj, a_class):
-    
-    """ Get a function that returns True if the object is an instance of a class that 
+
+    """ Returns True if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class
     """
-    
+
     if isinstance(obj, a_class):
         return True
     if hasattr(obj, '__class__') and issubclass(obj.__class__, a_class):
