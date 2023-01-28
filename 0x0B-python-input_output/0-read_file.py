64e971b3e81bfec7dfc86c2e6948ace680d75ea3
manuel <mnl.azs@gmail.com>
2023-01-28 22:45:08 +0000

    lol

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Funcion para leer un archivo
+Funcion que lee un archivo de texto
 """
 
 
 def read_file(filename=""):
     """
-    la funcion lee el archivo de texto e imprime su salida
-    """
+Funcion que lee un archivo de texto
+"""
 
-with open(filename, 'la_carpetica', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         print(file.read())
