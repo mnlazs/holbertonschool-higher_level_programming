0209c57d0c20ff9d7e59134b4e95e49f25049d63
manuel <mnl.azs@gmail.com>
2023-01-29 22:42:47 +0000

    lol

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+    """
+    function that creates an Object from a “JSON file
+    """
+import json
+    """function that creates an Object from a “JSON file
+    """
+    
+    
+def load_from_json_file(filename):
+    """funtion"""
+    
+    
+    with open(filename, 'r') as file:
+        json.dump(my_obj, file)
+        
+        
