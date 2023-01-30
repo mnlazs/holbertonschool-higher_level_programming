0d34c0783ea30372f2791510f01f4ab9e048930b
manuel <mnl.azs@gmail.com>
2023-01-30 00:14:04 +0000

    lol

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write a class Student
+"""
+
+
+class Student:
+    """
+    that defines a student by: Public instance attributes\
+first_name, last_name, age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
