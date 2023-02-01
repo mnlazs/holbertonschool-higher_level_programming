7fe31015b26203d38f0d3401b473bf1b7946a94c
manuel <mnl.azs@gmail.com>
2023-02-01 16:50:19 +0000

    lol

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,44 +1,13 @@
 #!/usr/bin/python3
-"""
-   6-base_geometry.py module
-   Class BaseGeometry.
-   Public instance method: \
-   def area(self): that raises an Exception with\
-   the message area() is not implemented.
-   Public instance method:\
-   def integer_validator(self, name, value):\
-   that validates value
-"""
-
-
-class BaseGeometry:
-    """Class BaseGeometry"""
-
-    def area(self):
-        """Public instance method: def area(self): \
-           that raises an Exception with\
-           the message area() is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method:\
-           def integer_validator(self, name, value):\
-           that validates value"""
-        if type(value) != int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+"""module for Rectangle, class what inherits from BG - no test cases"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle that inherits from BaseGeometry"""
+    """class rectangle is a subclass of BG"""
     def __init__(self, width, height):
-        """Instantiation with width and height: \
-        def __init__(self, width, height)
-        width and height must be positive integers,\
-        validated by integer_validator"""
-
+        """init method"""
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
