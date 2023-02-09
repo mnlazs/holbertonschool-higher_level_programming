9600b3d27d21d0ae6fc6730b7c4ac0e6274e601d
mnlazs <mnl.azs@gmail.com>
2023-02-09 15:41:07 -0800

    adding new file

--- 0x00-SQL_introduction/2-remove_database.sql
+++ 0x00-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- 0x02. SQL - Introduction, task 2. delete the database
 -- script that deletes the database hbtn_0c_0
-DELETE DATABASE IF NOT EXISTS hbtn_0c_0;
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
