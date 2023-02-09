92836b850294a0cf070f1b25a90fd344b2c36648
mnlazs <mnl.azs@gmail.com>
2023-02-09 14:42:03 -0800

    adding a new file

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 --0x01. SQL - Introduction, task 1. create databases
 --script that creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
