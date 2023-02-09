25721c63d57ba0869ef0b93e88b2d7b819a0c3f2
mnlazs <mnl.azs@gmail.com>
2023-02-09 14:00:33 -0800

    fixing errors

--- 0x00-SQL_introduction/0-list_databases.sql
+++ 0x00-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
---This is a comment
---Printing a script that list all databases
-SHOW SCHEMAS;
+-- 0x0D. SQL - Introduction, task 0. List databases
+-- Shows all current databases. Synonymous with SHOW SCHEMAS;.
+SHOW DATABASES;
