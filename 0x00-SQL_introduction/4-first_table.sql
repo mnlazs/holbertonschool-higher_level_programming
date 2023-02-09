1f7b71e0876038d14f69035152d9c841ec81e220
mnlazs <mnl.azs@gmail.com>
2023-02-09 15:55:53 -0800

    adding new file

---
+++ 0x00-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- 0x04. SQL - Introduction, task 4. create a new table.
+-- script that creates a table called first_table
+CREATE TABLE IF NOT EXISTS first_table
+(
+id INT,
+name VARCHAR(256)
+);
