812d30dedaa150c8a47eba5ee752d56e36f0f77e
mnlazs <mnl.azs@gmail.com>
2023-02-10 08:52:42 -0800

    adding a new file

--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD USER IF NOT EXISTS 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
