9e46c59e4aa4e271b6c6ea552e8df64a4ec2071c
mnlazs <mnl.azs@gmail.com>
2023-02-09 17:02:28 -0800

    adding a file

--- 0x00-SQL_introduction/11-best_score.sql
+++ 0x00-SQL_introduction/11-best_score.sql
@@ -1,5 +1,6 @@
 -- 0x11. SQL - Introduction, task 11. 
 -- script that lists all records 
-SELECT * score,
+SELECT score, name
 FROM second_table
-ORDER BY score >=10;
+WHERE score >=10;
+ORDER BY score DESC:
