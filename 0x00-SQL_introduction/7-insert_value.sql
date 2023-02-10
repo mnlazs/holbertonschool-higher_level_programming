a3ab55df6d10f1f9527675d3504fc3ae310f4a90
mnlazs <mnl.azs@gmail.com>
2023-02-09 16:34:12 -0800

    adding a file

--- 0x00-SQL_introduction/7-insert_value.sql
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- 0x07. SQL - Introduction, task 7. Inserting a new row
 -- inserts a new row in the table first_table
 INSERT INTO first_table ("id", "name")
-VALUES ("89", "Best School");
+VALUES (89, "Best School");
