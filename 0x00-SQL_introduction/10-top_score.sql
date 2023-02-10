3fe9a5b3d4cf6e7bbc088441c1a716e72583e926
mnlazs <mnl.azs@gmail.com>
2023-02-09 16:56:40 -0800

    adding a file

--- 0x00-SQL_introduction/10-top_score.sql
+++ 0x00-SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
 -- 0x10. SQL - Introduction, task 10. 
 -- script that lists all records of the second_table
-SELECT * FROM second_table;
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
