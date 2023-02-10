3655eabba4ada461f58ad81861233782ebcefd2c
mnlazs <mnl.azs@gmail.com>
2023-02-10 08:41:30 -0800

    adding a new file

--- 0x01-SQL_more_queries/0-privileges.sql
+++ 0x01-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- 0x0E. SQL - More queries, task 0. My privileges!
--- Lists all priviledges for users `user_0d_1` and `user_0d_2`
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
