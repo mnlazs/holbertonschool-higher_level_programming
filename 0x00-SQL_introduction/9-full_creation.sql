f1a73a0de24b4005c5fcae40685a3859e5509235
mnlazs <mnl.azs@gmail.com>
2023-02-09 16:51:36 -0800

    adding a file

--- 0x00-SQL_introduction/9-full_creation.sql
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- 0x09. SQL - Introduction, task 9. Full creation
 -- script that creates a table second_table in the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table 
-(
+CREATE TABLE IF NOT EXISTS second_table (
     id = INT,
     name = VARCHAR(256),
     score = INT
