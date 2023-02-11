e4727361a0cd0cb673a93c9ab8d01b0afa88b1e5
mnlazs <mnl.azs@gmail.com>
2023-02-11 10:27:57 -0800

    adding a new file

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -1,11 +1,6 @@
 -- 0x0E. SQL - More queries, task 7.
 -- a script that creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
--- Creates table `states` with fields `id` and `name`.
-CREATE TABLE IF NOT EXISTS cities (
-       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       state_id INT NOT NULL,
-       name VARCHAR(256) NOT NULL,
-       FOREIGN KEY (state_id) REFERENCES states(id)
-); 
+SELECT id, name
+FROM cities
+WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
