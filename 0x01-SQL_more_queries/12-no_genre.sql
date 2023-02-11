ab6d38c1906ef58f04011e47d35f3b9b02ecd959
mnlazs <mnl.azs@gmail.com>
2023-02-11 15:47:00 -0800

    adding a new file

--- 0x01-SQL_more_queries/12-no_genre.sql
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,8 @@
--- 0x0E. SQL - More queries, task 12
--- script that lists all shows contained in hbtn_0d_tvshows 
+-- 0x0E. SQL - More queries, task 12. No genre 
+-- Lists all shows in `hbtn_0d_tvshows` without genre by title, genre.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
