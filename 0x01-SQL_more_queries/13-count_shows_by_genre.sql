3efcb99b71a7c2e949dfa0d7a816900cae83db7c
mnlazs <mnl.azs@gmail.com>
2023-02-11 15:54:39 -0800

    adding a new file

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- 0x0E. SQL - More queries, task 13. No genre 
 -- Lists all shows in `hbtn_0d_tvshows` without genre by title, genre.
-SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM genres
-LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING number_of_shows > 0
+SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_genres.id = genre_id
+GROUP BY genre_id
 ORDER BY number_of_shows DESC;
