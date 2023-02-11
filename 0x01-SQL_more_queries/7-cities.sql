b27fd72c8543f74f109a2e04eb3f8056f51e8528
mnlazs <mnl.azs@gmail.com>
2023-02-11 10:23:29 -0800

    adding a new file

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -4,6 +4,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 -- Creates table `states` with fields `id` and `name`.
 CREATE TABLE IF NOT EXISTS cities (
-       id INT UNIQUE NOT NULL AUTO_INCREMENT FOREIGN KEY,
-       name VARCHAR(256) NOT NULL
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id)
 ); 
