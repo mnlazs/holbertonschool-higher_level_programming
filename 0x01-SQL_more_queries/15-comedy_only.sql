305075eda8082eebf8f6e3ec77ad7ff464b8ec15
mnlazs <mnl.azs@gmail.com>
2023-02-11 16:24:29 -0800

    adding a new file

--- 0x01-SQL_more_queries/15-comedy_only.sql
+++ 0x01-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
--- 0x0E. SQL - More queries, task 14. My genres 
+-- 0x0E. SQL - More queries, task 15. My genres 
 -- Lists all genres of show `Dexter` in `hbtn_0d_tvshows`. DB in args.
-SELECT tv_genres.name
+SELECT tv_shows.title
 FROM tv_show_genres
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC; 
