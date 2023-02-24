8b49ad6f50c747a5b4f2b83b81d2059e8eb93af5
mnlazs <mnl.azs@gmail.com>
2023-02-24 11:32:28 -0800

    adding new file

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -1,10 +1,14 @@
+
+
 #!/usr/bin/python3
 """
-Script that lists all `cities` from database 'hbtn_0e_usa'
+Script that lists all `cities` in the `cities` table of `hbtn_0e_4_usa`
+where the city's state matches the argument `state name`.
 Arguments:
     mysql username (str)
     mysql password (str)
     database name (str)
+    state name (str)
 """
 
 import sys
@@ -14,17 +18,19 @@ if __name__ == "__main__":
     mySQL_u = sys.argv[1]
     mySQL_p = sys.argv[2]
     db_name = sys.argv[3]
-    
+
     state_name = sys.argv[4]
 
     # By default, it will connect to localhost:3306
     db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
 
-    cur.execute("SELECT cities.name FROM cities JOIN states ON\
-    cities.state_id = states.id AND states.name = %s ORDER BY cities.id ASC")
+    cur.execute("SELECT c.name \
+                 FROM cities c INNER JOIN states s \
+                 ON c.state_id = s.id WHERE s.name = %s\
+                 ORDER BY c.id", (state_name, ))
     rows = cur.fetchall()
 
     for i in range(len(rows)):
-        print(rows [i][0], end=", " if i + 1 < len(rows) else "")
+        print(rows[i][0], end=", " if i + 1 < len(rows) else "")
     print("")
