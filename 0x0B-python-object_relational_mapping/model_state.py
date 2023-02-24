3f2644d1ba3f2e2a132dab01619febd581ecf343
mnlazs <mnl.azs@gmail.com>
2023-02-24 11:50:04 -0800

    adding new file

---
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""list all state objects from the database
+"""
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Represents a state for a MySQL database.
+
+    Args:
+       __tablename__ (str): The name of the MySQL table to store States.
+        id (sqlalchemy.Integer): The state's id.
+        name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+pymysql://root:@localhost:3306/testdb', echo=True)
+    Base.metadata.create_all(engine)
+
