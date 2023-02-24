30551a30bbbddf65b1d0c239071019bdeedf1af8
mnlazs <mnl.azs@gmail.com>
2023-02-24 12:14:06 -0800

    adding new file

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects from the database 'hbtn_0e_6_usa'
+Script that lists all lists all `State`
+objects from the database `hbtn_0e_6_usa`.
 Arguments:
     mysql username (str)
     mysql password (str)
@@ -13,20 +14,21 @@ from sqlalchemy.orm import Session
 from sqlalchemy.engine.url import URL
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     mySQL_u = sys.argv[1]
     mySQL_p = sys.argv[2]
     db_name = sys.argv[3]
 
-url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
            'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
 
-engine = create_engine(URL(**url), pool_pre_ping=True)
-Base.metadata.create_all(engine)
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-session = Session(bind=engine)
+    session = Session(bind=engine)
 
-q = session.query(State).order_by(State.id)
+    q = session.query(State).order_by(State.id)
 
-for instance in q:
+    for instance in q:
         print("{}: {}".format(instance.id, instance.name))
