8dc2912cb707aa80e914d5ce3fd1fa7a7361ee80
mnlazs <mnl.azs@gmail.com>
2023-02-24 11:24:42 -0800

    adding new file

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name,
-                FROM cities JOIN states ON cities.state_id = states.id
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities JOIN states ON cities.state_id = states.id \
                 WHERE states.name = %s")
     rows = cur.fetchall()
 
