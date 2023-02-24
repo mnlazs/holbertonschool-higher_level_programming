12c65ec379d5ada5ad8011bb8f83097b75e80664
mnlazs <mnl.azs@gmail.com>
2023-02-24 11:29:14 -0800

    adding new file

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -14,15 +14,16 @@ if __name__ == "__main__":
     mySQL_u = sys.argv[1]
     mySQL_p = sys.argv[2]
     db_name = sys.argv[3]
+    
     state_name = sys.argv[4]
 
     # By default, it will connect to localhost:3306
     db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name \
-                FROM cities JOIN states ON cities.state_id = states.id \
-                WHERE states.name = %s")
+    cur.execute("SELECT cities.name FROM cities JOIN states ON\
+    cities.state_id = states.id AND states.name = %s ORDER BY cities.id ASC")
+    
     rows = cur.fetchall()
 
     for row in rows:
