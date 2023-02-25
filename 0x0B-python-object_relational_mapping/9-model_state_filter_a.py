7db1f0fca932c751abcb2954974dcfd4606a54ab
mnlazs <mnl.azs@gmail.com>
2023-02-25 13:44:01 -0800

    first obj

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-# lists all State objects that contain the letter a from a database
+"""
+Script that lists all `State` objects that contain
+the letter `a` from the database `hbtn_0e_6_usa`.
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
 
 
 if __name__ == "__main__":
-    from model_state import Base, State
-    from sys import argv
-    import sqlalchemy
-    from sqlalchemy.engine.url import URL
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
 
-    mysql = {'drivername': 'mysql+mysqldb',
-             'host': 'localhost',
-             'port': '3306',
-             'username': argv[1],
-             'password': argv[2],
-             'database': argv[3],
-             }
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
 
-    url = URL(**mysql)
-
-    engine = create_engine(url, pool_pre_ping=True)
+    engine = create_engine(URL(**url), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    session = Session(bind=engine)
 
-    query = session.query(State).filter(State.name.like('%a%'))\
-                                .order_by(State.id)
-    for r in query.all():
-        print("{}: {}".format(r.id, r.name))
+    q = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
 
-    session.close()
+    for instance in q:
+        print("{}: {}".format(instance.id, instance.name))
