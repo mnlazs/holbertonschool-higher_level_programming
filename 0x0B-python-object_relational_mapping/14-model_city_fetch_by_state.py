159b04722a6957c9c55c25d0b4e5def7366b0250
mnlazs <mnl.azs@gmail.com>
2023-02-27 08:28:59 -0800

    loading

--- 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-"""
-Script that prints all City objects from the database hbtn_0e_14_usa
-"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+""" prints all City objects from the database"""
 
-if __name__ == '__main__':
-    # create connection to the database
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database))
 
-    # create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
+if __name__ == "__main__":
+    from sqlalchemy.engine import create_engine
+    from sqlalchemy.engine.url import URL
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+    from model_city import City
+    from sys import argv
 
-    # query all City objects and print their information
-    for city, state in session.query(City, State).filter(City.state_id == State.id)\
-            .order_by(City.id).all():
-        print('{}: ({}) {}'.format(state.name, city.id, city.name))
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': argv[1],
+          'password': argv[2],
+          'database': argv[3]}
 
-    # close the session
+    url = URL(**db)
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state, city in session.query(State, City)\
+                              .filter(State.id == City.state_id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
--- 0x0B-python-object_relational_mapping/model_city.py
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""list all cities objects from the database
-"""
-
-from sqlalchemy import Column, Integer, String, ForeignKey
+""" class Model """
+from sqlalchemy import Integer, String, Column, ForeignKey
 from model_state import Base
- """Represents a state for a MySQL database.
 
-    Args:
-       __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-    """
 
 class City(Base):
+
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
