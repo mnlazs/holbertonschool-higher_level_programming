aa5b585e26c4d6288e7eb3fe3b6c0efb2fe90cc6
Your Name <you@example.com>
2023-03-09 22:59:40 +0000

    lol

---
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script
+"""
+
+import requests
+
+url = 'https://intranet.hbtn.io/status'
+response = requests.get(url)
+
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
