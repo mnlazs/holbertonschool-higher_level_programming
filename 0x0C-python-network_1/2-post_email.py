d7bb4b92c34e3156bde37fde1937a55097bfe408
Your Name <you@example.com>
2023-03-09 22:41:11 +0000

    lol

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""POST"""
 import urllib.parse
 import urllib.request
 import sys
@@ -11,3 +12,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data=data, method='POST')
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
+
