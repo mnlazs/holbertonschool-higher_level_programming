61b79d6b1358d03dd43b7a63a6d29caef3ffc87e
Your Name <you@example.com>
2023-03-09 22:33:59 +0000

    lol

---
+++ 0x0C-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
