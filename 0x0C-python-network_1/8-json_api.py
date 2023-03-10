fad4394c0d14eac9f3126e7e7a28d667f6dbebdd
Your Name <you@example.com>
2023-03-10 19:07:27 +0000

    update

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Search API
+"""
+
+
+import requests
+import sys
+
+if len(sys.argv) > 1:
+    letter = sys.argv[1]
+else:
+    letter = ""
+
+response = requests.post("http://0.0.0.0:5000/search_user", data={'q': letter})
+
+try:
+    data = response.json()
+    if data:
+        print("[{}] {}".format(data.get("id"), data.get("name")))
+    else:
+        print("ningún resultado")
+except ValueError:
+    print("No es un JSON válido")
