cc5d864160ba2578fa619ed0ce1bd35ae34b0ca3
Your Name <you@example.com>
2023-03-10 18:54:03 +0000

    lol

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""
-script to take in a URL input, sends a request to the URL, display response
-"""
-
+"""ERROR"""
 
 if __name__ == '__main__':
     from sys import argv
     import requests
 
-    try:
-        req = requests.get(argv[1])
-        if req:
-            print(req.content.decode('utf-8'))
-        elif req.status_code >= 400:
-            print("Error code: {}".format(req.status_code))
-    except KeyError:
-        pass
+response = requests.get(url)
+
+if response.status_code >= 400:
+        print("Error code:", response.status_code)
+else:
+        print(response.text)
