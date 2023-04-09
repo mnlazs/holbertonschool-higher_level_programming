d6c78fdb5f1bfbaf25d93caaef70da5fb7a4b097
mnlazs <mnl.azs@gmail.com>
2023-04-09 10:00:16 -0700

    javascript

--- 0x00-javascript-warm_up/4-concat.js
+++ 0x00-javascript-warm_up/4-concat.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 // Prints messages depending..
-const args = process.argv.slice(2);
 
-if (args.length >= 2) {
-  console.log(`${args[0]} is ${args[1]}`);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
