eb46f57364ea1f590e8ea331dee15f51255c2257
mnlazs <mnl.azs@gmail.com>
2023-04-09 10:36:57 -0700

    8-square.js

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ if (!Number.isInteger(parseInt(arg))) {
   console.log('Missing size');
 } else {
   const n = parseInt(arg);
-  let square = "";
+  let square = '';
 
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
