552f8a21c9cf38d5d118cffa26c1541aca14c55a
mnlazs <mnl.azs@gmail.com>
2023-04-09 09:41:15 -0700

    javaScript

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 // Prints messages depending..
 
 if (process.argv.length === 2) {
-    console.log("No argument");
+    console.log('No argument');
   } else if (process.argv.length === 3) {
-    console.log("Argument found");
+    console.log('Argument found');
   } else {
-    console.log("Arguments found");
+    console.log('Arguments found');
   }
   
