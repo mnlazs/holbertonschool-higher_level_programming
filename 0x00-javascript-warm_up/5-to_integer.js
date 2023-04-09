873ddbdb3efb4650cf68694d00c48dafa7638b7c
mnlazs <mnl.azs@gmail.com>
2023-04-09 10:06:51 -0700

    5-to_integer.js

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -6,7 +6,5 @@ const arg = process.argv[2];
 if (Number.isInteger(Number(arg))) {
   console.log(`My number: ${parseInt(arg, 10)}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
-
-
