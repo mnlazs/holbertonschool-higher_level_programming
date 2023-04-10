b5c748be5572e431bb566f72bb40e1260d605524
mnlazs <mnl.azs@gmail.com>
2023-04-10 11:34:50 -0700

    3-rectangle.js

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,24 @@
 #!/usr/bin/node
 // Write a class Rectangle that
 module.exports = class Rectangle {
-    constructor (width, height) {
-      if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
-        this.width = width;
-        this.height = height;
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      console.log('Empty rectangle');
+    } else {
+      let row = '';
+      for (let i = 0; i < this.width; i++) {
+        row += 'X';
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(row);
       }
     }
-    print() {
-        if (!this.width || !this.height) {
-          console.log('Empty rectangle');
-        } else {
-          let row = '';
-          for (let i = 0; i < this.width; i++) {
-            row += 'X';
-          }
-          for (let i = 0; i < this.height; i++) {
-            console.log(row);
-          }
-        }
-      }
   }
+};
