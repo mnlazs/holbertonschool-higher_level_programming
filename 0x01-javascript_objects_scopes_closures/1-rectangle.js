26d16d5f31fc2f5663423aaee5f26d17557692d8
mnlazs <mnl.azs@gmail.com>
2023-04-10 11:01:48 -0700

    1-rectangle.js

--- 0x01-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Write a class Rectangle that
-class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
   }
-}
+};
