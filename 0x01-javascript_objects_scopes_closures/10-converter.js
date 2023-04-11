7b6dff2f60fb56e531fbd33a51ecdcf99ec6d44f
mnlazs <mnl.azs@gmail.com>
2023-04-11 09:15:55 -0700

    10-converter.js

---
+++ 0x01-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Converts a number from base 10 to another base
+
+exports.converter = function (base) {
+    function myConverter (n) {
+      return n.toString(base);
+    }
+  
+    return myConverter;
+  };
