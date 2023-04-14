0985f7d3ae45f505f7df11144ae86c77a9e44284
Manuel <mnl.azs@gmail.com>
2023-04-14 19:33:36 +0000

    4

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(url, function(error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
