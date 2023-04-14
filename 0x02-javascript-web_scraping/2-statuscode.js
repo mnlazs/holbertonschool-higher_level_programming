f8a4f4fb847917b2205b12d4a0a1262657b20cdf
Manuel <mnl.azs@gmail.com>
2023-04-14 19:27:23 +0000

    2-statuscode.js

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function(error, response) {
+request (url, function(error, response) {
   if (error) {
     console.error(error);
   } else {
