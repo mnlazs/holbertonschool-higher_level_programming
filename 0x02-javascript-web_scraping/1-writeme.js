225f4a88b0f57952d86ab66ed00365a057bf824e
Manuel <mnl.azs@gmail.com>
2023-04-14 19:12:28 +0000

    1-writeme.js

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-// Obtener el argumento de la ruta del archivo y la cadena a escribir
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-// Escribir la cadena en el archivo
-fs.writeFile(filePath, stringToWrite, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(`El archivo ${filePath} ha sido actualizado.`);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
