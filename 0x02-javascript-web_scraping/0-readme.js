994195a61555f43bf273a99e73feaafdba1c26f5
Your Name <you@example.com>
2023-04-14 17:54:01 +0000

    0-readme.js

---
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file_path = process.argv[2];
+
+fs.readFile(file_path, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
